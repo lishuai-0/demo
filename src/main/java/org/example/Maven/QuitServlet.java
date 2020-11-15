77b297c9a76b9f129653d0354d0d2812c5026e1d
lishuai-0 <47440880@qq.com>
2020-11-15 17:52:52 +0800

    第一次提交

--- src/main/java/org/example/Maven/QuitServlet.java
+++ src/main/java/org/example/Maven/QuitServlet.java
@@ -11,9 +11,6 @@ import java.io.IOException;
 public class QuitServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getSession().invalidate();
-        resp.sendRedirect("login.jsp");
-
 
     }
 }
